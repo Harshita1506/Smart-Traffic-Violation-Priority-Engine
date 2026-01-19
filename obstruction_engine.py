a18cf800124255fd5505c25fa02119bd39bdfd17
Harshita1506 <s.harshita1510@gmail.com>
2026-01-19 21:24:44 +0530

    Update obstruction_engine.py

---
+++ obstruction_engine.py
@@ -0,0 +1,11 @@
+class ObstructionEngine:
+    def __init__(self):
+        pass
+
+    def compute(self, emergency_routes, trajectories):
+        obstructions = []
+        for eid, route in emergency_routes.items():
+            # Check if any trajectory intersects this route
+            # Placeholder: return empty
+            pass
+        return obstructions
